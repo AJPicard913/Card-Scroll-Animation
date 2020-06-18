66e92aa004fb6c227c2d3a47f7511c3728a4c8fd
ajpicard913 <Contact@saucedesigns.io>
2020-06-18 09:29:51 -0400

    Initial Commit

---
+++ CardAnimation/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  CardAnimation
+//
+//  Created by AJ Picard on 6/18/20.
+//  Copyright © 2020 AJ Picard. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ CardAnimationTests/CardAnimationTests.swift
@@ -0,0 +1,34 @@
+//
+//  CardAnimationTests.swift
+//  CardAnimationTests
+//
+//  Created by AJ Picard on 6/18/20.
+//  Copyright © 2020 AJ Picard. All rights reserved.
+//
+
+import XCTest
+@testable import CardAnimation
+
+class CardAnimationTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
